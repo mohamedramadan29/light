49879e6886f3a4d8b5c581708e3e0ad7e8b259b9
mohamed <mr319242@gmail.com>
2023-08-17 12:06:24 +0300

    ىثص

--- include/navbar.php
+++ include/navbar.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php"><img style="width: 115px;" src="uploads/logo.png"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
